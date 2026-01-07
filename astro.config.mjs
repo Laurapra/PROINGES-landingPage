8f68269d97207a73b204f49392ae4c8397a56f90
Laurapra <laurapra17@gmail.com>
2026-01-07 13:38:33 -0500

    fix: add Vercel adapter for hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://proinges.com.co',
-  output: 'hybrid', 
-  adapter: vercel(), 
+  output: 'hybrid',
+  adapter: vercel(),
 });
